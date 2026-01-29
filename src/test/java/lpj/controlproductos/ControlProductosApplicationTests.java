dc21e74937a2ab538e50d01199524d1d09ddf26b
Elio Mari <eliomari48@gmail.com>
2026-01-29 18:19:07 -0300

    primer commit

---
+++ src/test/java/lpj/controlproductos/ControlProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package lpj.controlproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
