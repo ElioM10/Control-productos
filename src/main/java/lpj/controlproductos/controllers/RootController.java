a96c237fde3c21e9328efdb87e1cb702ce7f4142
Elio Marí <107126606+ElioM10@users.noreply.github.com>
2026-02-10 16:00:02 -0300

    feat: mejorar página de login

--- src/main/java/lpj/controlproductos/controllers/RootController.java
+++ src/main/java/lpj/controlproductos/controllers/RootController.java
@@ -1,35 +1,18 @@
 package lpj.controlproductos.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import lpj.controlproductos.model.Categoria;
-import lpj.controlproductos.model.Marca;
 import lpj.controlproductos.model.Negocio;
-import lpj.controlproductos.model.Producto;
-import lpj.controlproductos.services.interfaces.CategoriaService;
-import lpj.controlproductos.services.interfaces.MarcaService;
 import lpj.controlproductos.services.interfaces.NegocioService;
-import lpj.controlproductos.services.interfaces.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.w3c.dom.ls.LSInput;
 
 import java.util.List;
 
 @Controller
 @Slf4j
 public class RootController {
-
-    @Autowired
-    ProductoService productoService;
-
-    @Autowired
-    CategoriaService categoriaService;
-
-    @Autowired
-    MarcaService marcaService;
 
     @Autowired
     NegocioService negocioService;
@@ -39,11 +22,13 @@ public class RootController {
 
         List<Negocio> negocios = negocioService.listarNegocios();
 
-        model.addAttribute("negocios",negocios);
+        model.addAttribute("negocios", negocios);
 
         return "layout/index";
     }
 
-
-
+    @GetMapping("/login")
+    public String login() {
+        return "login/login";
+    }
 }
